6d0893a0b4eb59d452774e8833b9058faa412d88
Vlad Borovsky <xvorob@gmail.com>
2018-09-25 17:51:46 +0300

    EquationsSolved

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
 module.exports = function solveEquation(equation) {
-  // your implementation
-}
+	
+    const newArr = equation.split(' ');
+  
+    a=newArr[0];
+    diffOne=newArr[3];
+    b=newArr[4];
+    diffTwo=newArr[7];
+    c=newArr[8];
+  
+    if (diffOne==='-') {b=-b;}
+    if (diffTwo==='-') {c=-c;}
+  
+    d=Math.sqrt((Math.pow(b,2))-(4*a*c));
+  
+    if (d === 0) {x1=(-b)/(2*a); x2=x1;}
+  
+    x1=Math.round((-b+d)/(2*a));
+    x2=Math.round((-b-d)/(2*a));
+  
+    if (x1>x2) {return solution = [x2,x1];} else {return solution = [x1,x2];}
+  }
